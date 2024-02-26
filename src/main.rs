806604f548f71ca06fce225474daad4f53e32181
Christian Fischer <christian@chfi.se>
2024-02-26 14:46:27 -0600

    parse paf

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,151 @@
+use rustc_hash::FxHashMap;
 use std::borrow::Cow;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop,
     window::Window,
 };
+
+use std::io::prelude::*;
+
+use anyhow::anyhow;
+
+struct Queries {
+    //
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct PafLine<S> {
+    query_name: S,
+    query_seq_len: usize,
+    query_seq_start: usize,
+    query_seq_end: usize,
+
+    tgt_name: S,
+    tgt_seq_len: usize,
+    tgt_seq_start: usize,
+    tgt_seq_end: usize,
+
+    strand_rev: bool,
+    cigar: S,
+}
+
+#[derive(Debug, Default, Clone)]
+struct AlignedSeq {
+    // name of the given sequence
+    name: String,
+    // its length
+    len: usize,
+    // its rank among other seqs in the query or target set
+    rank: usize,
+    // its start offset in the global all-to-all alignment matrix
+    offset: usize,
+}
+
+// #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+// struct AlignedSeq {
+//     name: String,
+//     len: usize,
+// }
+
+#[derive(Default)]
+struct NameCache {
+    query_names: FxHashMap<String, usize>,
+    target_names: FxHashMap<String, usize>,
+}
+
+fn parse_paf_line<'a>(mut fields: impl Iterator<Item = &'a str>) -> Option<PafLine<&'a str>> {
+    let (query_name, query_seq_len, query_seq_start, query_seq_end) =
+        parse_name_range(&mut fields)?;
+    let strand = fields.next()?;
+    let (tgt_name, tgt_seq_len, tgt_seq_start, tgt_seq_end) = parse_name_range(&mut fields)?;
+
+    let cigar = fields.skip(3).find_map(|s| s.strip_prefix("cg:Z:"))?;
+
+    Some(PafLine {
+        query_name,
+        query_seq_len,
+        query_seq_start,
+        query_seq_end,
+
+        tgt_name,
+        tgt_seq_len,
+        tgt_seq_start,
+        tgt_seq_end,
+
+        strand_rev: strand == "-",
+        cigar,
+    })
+}
+
+fn parse_name_range<'a>(
+    mut fields: impl Iterator<Item = &'a str>,
+) -> Option<(&'a str, usize, usize, usize)> {
+    let name = fields.next()?;
+    let len = fields.next()?.parse().ok()?;
+    let start = fields.next()?.parse().ok()?;
+    let end = fields.next()?.parse().ok()?;
+    Some((name, len, start, end))
+}
+
+pub fn main() -> anyhow::Result<()> {
+    let mut args = std::env::args();
+    let paf_path = args.nth(1).ok_or(anyhow!("Path to PAF not provided"))?;
+
+    // parse paf
+    let reader = std::fs::File::open(paf_path).map(std::io::BufReader::new)?;
+
+    let mut names = NameCache::default();
+
+    let mut queries = Vec::<AlignedSeq>::new();
+    let mut targets = Vec::<AlignedSeq>::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        if let Some(paf_line) = parse_paf_line(line.split("\t")) {
+            let qi = names.query_names.len();
+            let ti = names.target_names.len();
+
+            let query_name = paf_line.query_name.to_string();
+            let target_name = paf_line.query_name.to_string();
+
+            names.query_names.insert(query_name.clone(), qi);
+            names.target_names.insert(target_name.clone(), ti);
+
+            queries.push(AlignedSeq {
+                name: query_name,
+                len: paf_line.query_seq_len,
+                ..AlignedSeq::default()
+            });
+            targets.push(AlignedSeq {
+                name: target_name,
+                len: paf_line.tgt_seq_len,
+                ..AlignedSeq::default()
+            });
+        }
+    }
+
+    let process_aligned = |aseqs: &mut [AlignedSeq]| {
+        let mut by_len = aseqs.iter().map(|a| a.len).enumerate().collect::<Vec<_>>();
+        by_len.sort_by_key(|(_id, len)| *len);
+
+        let mut offset = 0;
+
+        for (rank, (i, _len)) in by_len.into_iter().enumerate() {
+            let entry = &mut aseqs[i];
+            entry.rank = rank;
+            entry.offset = offset;
+            offset += entry.len;
+        }
+
+        offset
+    };
+
+    let target_len = process_aligned(&mut targets);
+    let query_len = process_aligned(&mut queries);
+
+    Ok(())
+}
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
     let mut size = window.inner_size();
