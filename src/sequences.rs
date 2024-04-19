10b4518f4cf9dad0d29bf03b7c54dc0bcfc78931
Christian Fischer <christian@chfi.se>
2024-04-19 11:52:31 -0500

    constructing dedicated Sequences struct with optional fasta

--- src/sequences.rs
+++ src/sequences.rs
@@ -8,9 +8,60 @@ pub struct SeqId(pub usize);
 pub struct Sequences {
     sequence_names: Arc<bimap::BiMap<String, SeqId>>,
 
-    seqs: Option<FxHashMap<SeqId, Vec<u8>>>,
+    sequences: Option<FxHashMap<SeqId, Vec<u8>>>,
 }
 
 impl Sequences {
-    // pub fn from_fasta(seq_names: &bimap::BiMap<String, SeqId>,
+    pub fn from_paf<'a>(
+        lines: impl IntoIterator<Item = &'a crate::PafLine<&'a str>>,
+    ) -> Option<Self> {
+        let mut seq_names = bimap::BiMap::new();
+
+        for line in lines {
+            //
+        }
+
+        Some(Sequences {
+            sequence_names: Arc::new(seq_names),
+            sequences: None,
+        })
+    }
+
+    pub fn from_paf_and_fasta<'a>(
+        paf_lines: impl IntoIterator<Item = &'a crate::PafLine<&'a str>>,
+        fasta_path: impl AsRef<std::path::Path>,
+    ) -> std::io::Result<Self> {
+        let mut seq_names = bimap::BiMap::new();
+        let mut seqs: Vec<Vec<u8>> = Vec::new();
+
+        let mut fasta_reader = std::fs::File::open(fasta_path)
+            .map(std::io::BufReader::new)
+            .map(noodles::fasta::Reader::new)?;
+
+        for record in fasta_reader.records() {
+            let record = record?;
+            let name = std::str::from_utf8(record.name()).map_err(|_| {
+                std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Sequence name was not valid UTF-8",
+                )
+            })?;
+        }
+
+        // for record in fasta_index {
+        //     fasta_reader.
+        //     //
+        // }
+
+        let seqs = seqs
+            .into_iter()
+            .enumerate()
+            .map(|(i, seq)| (SeqId(i), seq))
+            .collect::<FxHashMap<_, _>>();
+
+        Ok(Sequences {
+            sequence_names: Arc::new(seq_names),
+            sequences: Some(seqs),
+        })
+    }
 }
