6af40500a50b027f10151970e5162fa66325d779
Christian Fischer <christian@chfi.se>
2024-08-06 12:40:52 +0300

    (WIP) refactoring annotations

--- src/app/annotations.rs
+++ src/app/annotations.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::view::RenderLayers, sprite::MaterialMesh2dBundle};
 
 use crate::annotations::RecordListId;
 
@@ -14,23 +14,45 @@ impl Plugin for AnnotationsPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<LabelPhysics>()
             .init_resource::<AnnotationPainter>()
+            .init_resource::<Annotations>()
             .add_event::<LoadAnnotationFile>()
             .add_systems(Startup, setup)
             .add_systems(PreUpdate, load_annotation_file.pipe(prepare_annotations))
             .add_systems(
                 Update,
-                (update_annotation_labels, draw_annotations)
-                    .chain()
-                    .after(super::gui::menubar_system),
+                (update_annotation_regions, update_annotation_labels),
             );
+        // .add_systems(
+        //     Update,
+        //     (update_annotation_labels, draw_annotations)
+        //         .chain()
+        //         .after(super::gui::menubar_system),
+        // );
     }
 }
 
-// #[derive(Component)]
-// struct Annotation {
-//     record_list: RecordListId,
-//     list_index: usize,
-// }
+#[derive(Resource, Default, Deref)]
+pub struct Annotations(pub crate::annotations::AnnotationStore);
+
+#[derive(Component)]
+pub struct Annotation {
+    pub record_list: RecordListId,
+    pub list_index: usize,
+}
+
+#[derive(Component)]
+struct DisplayEntities {
+    query_region: Entity,
+    query_label: Entity,
+
+    target_region: Entity,
+    target_label: Entity,
+    // query_region: Option<Entity>,
+    // query_label: Option<Entity>,
+
+    // target_region: Option<Entity>,
+    // target_label: Option<Entity>,
+}
 
 #[derive(Event)]
 pub struct LoadAnnotationFile {
@@ -45,12 +67,27 @@ struct LabelPhysics(crate::annotations::physics::LabelPhysics);
 #[derive(Resource, Default)]
 pub struct AnnotationPainter(pub crate::annotations::draw::AnnotationPainter);
 
+#[derive(Resource)]
+struct DisplayHandles {
+    mesh: bevy::sprite::Mesh2dHandle,
+    // material: Handle<ColorMaterial>,
+}
+
 fn setup(
-    // mut commands: Commands,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Assets<ColorMaterial>>,
     viewer: Res<super::PafViewer>,
     mut load_events: EventWriter<LoadAnnotationFile>,
     mut label_physics: ResMut<LabelPhysics>,
 ) {
+    let mesh = meshes.add(Mesh::from(Rectangle::default()));
+    // let material = materials.add(ColorMaterial::default());
+    commands.insert_resource(DisplayHandles {
+        mesh: mesh.into(),
+        // material,
+    });
+
     label_physics.0.heightfields =
         crate::annotations::physics::AlignmentHeightFields::from_alignments(&viewer.app.alignments);
 
@@ -62,7 +99,132 @@ fn setup(
     }
 }
 
+fn load_annotation_file(
+    frame_count: Res<bevy::core::FrameCount>,
+    mut annotations: ResMut<Annotations>,
+    // mut annotation_painter: ResMut<AnnotationPainter>,
+    // mut viewer: ResMut<super::PafViewer>,
+    mut load_events: EventReader<LoadAnnotationFile>,
+) -> Vec<crate::annotations::AnnotationId> {
+    //
+    todo!();
+}
+
+fn prepare_annotations(
+    In(labels_to_prepare): In<Vec<crate::annotations::AnnotationId>>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+
+    display_handles: Res<DisplayHandles>,
+) {
+    for (list_id, entry_id) in labels_to_prepare {
+        // TODO color from annotation/name
+        let color_mat = materials.add(ColorMaterial::default());
+
+        let query_region = commands
+            .spawn((
+                RenderLayers::layer(1),
+                SpatialBundle::HIDDEN_IDENTITY,
+                MaterialMesh2dBundle {
+                    mesh: display_handles.mesh.clone(),
+                    material: color_mat.clone(),
+                    ..default()
+                },
+            ))
+            .id();
+        let target_region = commands
+            .spawn((
+                RenderLayers::layer(1),
+                SpatialBundle::HIDDEN_IDENTITY,
+                MaterialMesh2dBundle {
+                    mesh: display_handles.mesh.clone(),
+                    material: color_mat.clone(),
+                    ..default()
+                },
+            ))
+            .id();
+
+        // TODO labels
+        let query_label = commands.spawn(()).id();
+        let target_label = commands.spawn(()).id();
+
+        let mut annot_ent = commands.spawn(Annotation {
+            record_list: list_id,
+            list_index: entry_id,
+        });
+
+        annot_ent.insert(DisplayEntities {
+            query_region,
+            query_label,
+            target_region,
+            target_label,
+        });
+    }
+    //
+}
+
+fn update_annotation_regions(
+    alignment_view: Res<AlignmentViewport>,
+    annotations: Res<Annotations>,
+
+    display_ents: Query<(&Annotation, &DisplayEntities)>,
+    mut transforms: Query<&mut Transform>,
+) {
+    for (annot, entities) in display_ents.iter() {
+        // update region transforms (screenspace) based on current view
+        if let Ok(cmds) = transforms.get_mut(entities.query_region) {
+            // TODO
+        }
+
+        // TODO target region too
+    }
+}
+
 fn update_annotation_labels(
+    //
+    time: Res<Time>,
+    // viewer: Res<super::PafViewer>,
+    app_view: Res<AlignmentViewport>,
+    windows: Query<&Window>,
+
+    mut label_physics: ResMut<LabelPhysics>,
+    // mut annotation_painter: ResMut<AnnotationPainter>,
+) {
+
+    //
+}
+
+fn prepare_annotations_old(
+    // while we're using egui's fonts, which won't be available the first frame
+    In(labels_to_prepare): In<Vec<crate::annotations::AnnotationId>>,
+
+    frame_count: Res<bevy::core::FrameCount>,
+    viewer: Res<super::PafViewer>,
+
+    mut contexts: bevy_egui::EguiContexts,
+    mut annotation_painter: ResMut<AnnotationPainter>,
+    mut label_physics: ResMut<LabelPhysics>,
+) {
+    if labels_to_prepare.is_empty() || frame_count.0 == 0 {
+        return;
+    }
+
+    let ctx = contexts.ctx_mut();
+
+    let app = &viewer.app;
+
+    ctx.fonts(|fonts| {
+        label_physics.0.prepare_annotations(
+            &app.alignment_grid,
+            &app.annotations,
+            labels_to_prepare.into_iter(),
+            fonts,
+            &mut annotation_painter.0,
+        );
+    });
+}
+
+fn update_annotation_labels_old(
     //
     time: Res<Time>,
     viewer: Res<super::PafViewer>,
@@ -107,6 +269,7 @@ fn update_annotation_labels(
     label_physics.0.step(grid, dt, &viewport);
 }
 
+/*
 fn draw_annotations(
     mut contexts: bevy_egui::EguiContexts,
     mut annotation_painter: ResMut<AnnotationPainter>,
@@ -125,8 +288,9 @@ fn draw_annotations(
         &app_view.view,
     );
 }
+*/
 
-fn load_annotation_file(
+fn load_annotation_file_old(
     frame_count: Res<bevy::core::FrameCount>,
     mut annotation_painter: ResMut<AnnotationPainter>,
     mut viewer: ResMut<super::PafViewer>,
@@ -167,34 +331,4 @@ fn load_annotation_file(
     }
 
     labels_to_prepare
-}
-
-fn prepare_annotations(
-    // while we're using egui's fonts, which won't be available the first frame
-    In(labels_to_prepare): In<Vec<crate::annotations::AnnotationId>>,
-
-    frame_count: Res<bevy::core::FrameCount>,
-    viewer: Res<super::PafViewer>,
-
-    mut contexts: bevy_egui::EguiContexts,
-    mut annotation_painter: ResMut<AnnotationPainter>,
-    mut label_physics: ResMut<LabelPhysics>,
-) {
-    if labels_to_prepare.is_empty() || frame_count.0 == 0 {
-        return;
-    }
-
-    let ctx = contexts.ctx_mut();
-
-    let app = &viewer.app;
-
-    ctx.fonts(|fonts| {
-        label_physics.0.prepare_annotations(
-            &app.alignment_grid,
-            &app.annotations,
-            labels_to_prepare.into_iter(),
-            fonts,
-            &mut annotation_painter.0,
-        );
-    });
 }
