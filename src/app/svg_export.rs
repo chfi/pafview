c30ecdac1ad44920311fd1bb8f318a76a92c8ee1
Christian Fischer <christian@chfi.se>
2025-01-13 12:52:09 -0600

    include grid in SVG export

--- src/app/svg_export.rs
+++ src/app/svg_export.rs
@@ -1,12 +1,19 @@
-use bevy::prelude::*;
+use bevy::{math::DVec2, prelude::*};
 use bevy_egui::{EguiClipboard, EguiContexts};
 
 use nalgebra::{OPoint, Point2};
-use svg::node::element::{path, Path};
+use svg::node::element::{
+    path::{self, Data},
+    Path,
+};
 use time::OffsetDateTime;
 
-use super::render::sampled_lines::{
-    pipeline::PolylineVertices, AlignmentCollisionLines, SampledAlignmentViewer, VertexSamplingTask,
+use super::{
+    alignments::{layout::SeqPairLayout, AlignmentLayoutQuery},
+    render::sampled_lines::{
+        pipeline::PolylineVertices, AlignmentCollisionLines, SampledAlignmentViewer,
+        VertexSamplingTask,
+    },
 };
 
 pub struct SvgExportPlugin;
@@ -24,11 +31,17 @@ fn export_svg_screenshot(
         Without<VertexSamplingTask>,
     >,
 
+    layouts: AlignmentLayoutQuery,
+
     keyboard: Res<ButtonInput<KeyCode>>,
 ) {
     if !keyboard.just_pressed(KeyCode::F12) {
         return;
     }
+
+    let Some(layout) = layouts.layout_assets.get(&layouts.default_layout.layout) else {
+        return;
+    };
 
     println!("exporting view as SVG...");
 
@@ -45,8 +58,20 @@ fn export_svg_screenshot(
 
     let mut document = svg::Document::new().set("viewBox", (0.0, 0.0, w, h));
 
+    let grid_path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 0.5)
+        .set(
+            "d",
+            grid_paths_in_view(layout, &params.view, params.canvas_size.as_vec2()),
+        );
+
+    document = document.add(grid_path);
+
     for (key, polyline) in lines.polylines.iter() {
         let mut points = polyline.vertices().iter().map(|p| (p.x as f32, p.y as f32));
+        // .map(|p| (p.x as f32, h - p.y as f32));
 
         let Some(p0) = points.next() else {
             continue;
@@ -62,7 +87,7 @@ fn export_svg_screenshot(
         let path = Path::new()
             .set("fill", "none")
             .set("stroke", "black")
-            .set("stroke-width", 8)
+            .set("stroke-width", 5)
             .set("d", path_data.close());
 
         document = document.add(path);
@@ -77,8 +102,7 @@ fn export_svg_screenshot(
 
     let (y, mon, d) = time.to_calendar_date();
     let (h, min, s) = time.to_hms();
-
-    let ymdhms = format!("{y}-{mon}-{d}-{h}:{min}:{s}");
+    let ymdhms = format!("{y}-{mon:02}-{d:02}-{h:02}-{min:02}-{s:02}");
 
     // TODO proper file name (include PAF name, region (configurable))
     let file_name = format!("pafview-{ymdhms}.svg");
@@ -91,4 +115,54 @@ fn export_svg_screenshot(
             log::error!("Error saving SVG: {e}");
         }
     }
+}
+
+fn grid_paths_in_view(
+    // document: &mut Document,
+    tile_layout: &SeqPairLayout,
+    view: &crate::view::View,
+    canvas_size: Vec2,
+) -> path::Data {
+    let mut data = Data::new();
+
+    let vis_tiles = tile_layout
+        .layout_qbvh
+        .aabbs_in_rect(view.center(), view.size() * 0.5);
+
+    for tile in vis_tiles.iter() {
+        let Some(aabb) = tile_layout.aabbs.get(tile) else {
+            continue;
+        };
+
+        let center = view.map_world_to_screen(canvas_size, aabb.center().coords.data.0[0]);
+        let size = DVec2::from(aabb.extents().data.0[0]) * (canvas_size.x as f64 / view.width());
+
+        let p = Vec2::new(center.x, center.y);
+        let size = size.as_vec2();
+        let halfsize = size * 0.5;
+        let dx = Vec2::X * size;
+        let dy = Vec2::Y * size;
+
+        let p0 = p - halfsize;
+        let p1 = p0 + dx;
+        let p2 = p1 + dy;
+        let p3 = p2 - dx;
+
+        let f = |p: Vec2| (p.x, p.y);
+
+        data = data
+            .move_to(f(p0))
+            .line_to(f(p1))
+            .line_to(f(p2))
+            .line_to(f(p3))
+            .close();
+
+        // .line_to(())
+        // data.move_to()
+        // data.move_to(center)
+        // let c = DVec2::from(aabb.center().coords.
+        // let screen_center = aabb.center()
+    }
+
+    data
 }
